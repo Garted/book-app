3f4f5df2c17a8caddfae1f8d651689a738ff4597
Eugeny Kurnysh <garted93@gmail.com>
2024-10-02 16:03:47 +0300

    comit

--- frontend/src/components/Circle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Circle = () => {
-    return <div>Circle Modify</div>;
-};
-
-export default Circle;
--- frontend/src/components/Ret.js
+++ frontend/src/components/Ret.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Ret = () => {
-    return <div>Ret</div>;
+    return <div>Retryasdasdas</div>;
 };
 
 export default Ret;
